b842c36034e13b593aa0d4f80d6f3b8f3996a061
samudraam <112435495+samudraam@users.noreply.github.com>
2025-02-03 00:05:20 -0600

    Updating Movement + Animation

--- Assets/Code/PlayerCode/playerController.cs
+++ Assets/Code/PlayerCode/playerController.cs
@@ -3,34 +3,63 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-namespace PlayerCode{
-public class playerController : MonoBehaviour
+namespace PlayerCode
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   {
+      Rigidbody2D playerRB;
+      Animator animator;
+      float speed = 5f;
+      private Vector3 originalScale;
 
-    // Update is called once per frame
-    void Update()
-    {
-     //move up (to be changed to jump)  
-     if(Input.GetKey(KeyCode.UpArrow)){
-        transform.position += new Vector3(0,0.2f,0);
-     }
-     //move down (to be changed to fall through platforms/fastfall)  
-     if(Input.GetKey(KeyCode.DownArrow)){
-        transform.position += new Vector3(0,-0.2f,0);
-     }
-     //move backwards
-     if(Input.GetKey(KeyCode.LeftArrow)){
-        transform.position += new Vector3(-0.2f,0,0);
-     }
-     //move forewards
-     if(Input.GetKey(KeyCode.LeftArrow)){
-        transform.position += new Vector3(0.2f,0,0);
-     }
-    }
-}
+      // Start is called before the first frame update
+      void Start()
+      {
+         playerRB = GetComponent<Rigidbody2D>();
+         animator = GetComponent<Animator>();
+         originalScale = transform.localScale;
+      }
+
+      // Update is called once per frame
+      void Update()
+      {
+         // //move up (to be changed to jump)  
+         // if (Input.GetKey(KeyCode.UpArrow))
+         // {
+         //    transform.position += new Vector3(0, 0.2f, 0);
+         // }
+         // //move down (to be changed to fall through platforms/fastfall)  
+         // if (Input.GetKey(KeyCode.DownArrow))
+         // {
+         //    transform.position += new Vector3(0, -0.2f, 0);
+         // }
+         // //move backwards
+         // if (Input.GetKey(KeyCode.LeftArrow))
+         // {
+         //    transform.position += new Vector3(-0.2f, 0, 0);
+         // }
+         // //move forewards
+         // if (Input.GetKey(KeyCode.LeftArrow))
+         // {
+         //    transform.position += new Vector3(0.2f, 0, 0);
+         // }
+
+         float moveInput = Input.GetAxisRaw("Horizontal");
+
+         //jump on key down
+         if (Input.GetKeyDown(KeyCode.UpArrow))
+         {
+            playerRB.AddForce(Vector2.up * 5f, ForceMode2D.Impulse);
+         }
+
+         playerRB.velocity = new Vector2(moveInput * speed, playerRB.velocity.y);
+
+         animator.SetFloat("moving", Mathf.Abs(playerRB.velocity.x));
+
+         if (moveInput != 0)
+         {
+            transform.localScale = new Vector3(originalScale.x * Mathf.Sign(moveInput), originalScale.y, originalScale.z);
+         }
+
+      }
+   }
 }
