4afcd782a83c7331b3ffb1f111db248eebcdb48c
samudraam <112435495+samudraam@users.noreply.github.com>
2025-04-21 14:53:21 -0500

    level end

--- Assets/Code/Game/LevelEnd.cs
+++ Assets/Code/Game/LevelEnd.cs
@@ -4,19 +4,33 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelEnd : MonoBehaviour
-
-
 {
     [Tooltip("Name of the scene to load next")]
-    public string nextSceneName = "Level1";
+    public string nextSceneName = "Level2";
+
+    [Tooltip("Index of the level being unlocked (e.g. 2 for Level2)")]
+    public int nextLevelIndex = 2;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             Debug.Log("Level complete!");
+
+            UnlockNextLevel(nextLevelIndex);
             SceneManager.LoadScene(nextSceneName);
         }
     }
+
+    private void UnlockNextLevel(int index)
+    {
+        int unlocked = PlayerPrefs.GetInt("UnlockedLevel", 1);
+        if (index > unlocked)
+        {
+            PlayerPrefs.SetInt("UnlockedLevel", index);
+            PlayerPrefs.Save();
+        }
+    }
 }
+
 
--- Assets/Code/Game/LevelMenu.cs
+++ Assets/Code/Game/LevelMenu.cs
@@ -2,12 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 namespace Game
 {
-
-
     public class LevelMenu : MonoBehaviour
     {
+        public Button[] levelButtons; 
+
+        void Start()
+        {
+            int unlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 1);
+
+            for (int i = 0; i < levelButtons.Length; i++)
+            {
+                levelButtons[i].interactable = (i < unlockedLevel);
+            }
+        }
+
         public void OpenLevel(int levelId)
         {
             string levelName = "Level" + levelId;
