1cddee3772cee1f562db79ead166cc5ae58b8c17
Zihan Kou <164995465+gswskyle@users.noreply.github.com>
2025-04-21 22:09:40 -0500

    Add pick coin sound effect

--- Assets/Code/Coins/CoinPickup.cs
+++ Assets/Code/Coins/CoinPickup.cs
@@ -5,15 +5,16 @@ using PlayerCode;
 
 public class CoinPickup : MonoBehaviour
 {
-
+    public AudioClip coinSound;
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
-
         if (collision.CompareTag("Player"))
         {
             playerController.numberOfCoins++; 
             PlayerPrefs.SetInt("Coins", playerController.numberOfCoins);
             Destroy(gameObject);
+            AudioSource.PlayClipAtPoint(coinSound,transform.position);
             Debug.Log("coin picked up!");
         }
     }
